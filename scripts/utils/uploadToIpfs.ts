c0e5f7dba96885470f414a6373065f4428829a2e
Jacob Tucker <tsnakejake@aol.com>
2024-10-02 17:11:49 -0400

    fix: ts error in uploadToIpfs.ts

--- scripts/utils/uploadToIpfs.ts
+++ scripts/utils/uploadToIpfs.ts
@@ -1,6 +1,6 @@
 const pinataSDK = require('@pinata/sdk')
 
-export async function uploadJSONToIPFS(jsonMetadata): Promise<string> {
+export async function uploadJSONToIPFS(jsonMetadata: any): Promise<string> {
     const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT })
     const { IpfsHash } = await pinata.pinJSONToIPFS(jsonMetadata)
     return IpfsHash
