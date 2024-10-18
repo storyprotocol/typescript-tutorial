5d2b1120916f2e9b6f33533d0d1a203acf4344ef
jacob-tucker <jacobtucker818@gmail.com>
2024-10-18 15:14:13 -0700

    remove var

--- scripts/utils/utils.ts
+++ scripts/utils/utils.ts
@@ -20,11 +20,3 @@ export const RPCProviderUrl = process.env.RPC_PROVIDER_URL || 'https://testnet.s
 // This address must be whitelisted by the protocol. You can see the
 // currently whitelisted addresses here: https://docs.story.foundation/docs/royalty-module#whitelisted-revenue-tokens
 export const CurrencyAddress: Address = (process.env.CURRENCY_ADDRESS as Address) || '0x91f6F05B08c16769d3c85867548615d270C42fC7'
-
-export const mintContractApi = {
-    inputs: [{ internalType: 'address', name: 'to', type: 'address' }],
-    name: 'mint',
-    outputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-}
