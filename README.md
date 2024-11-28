a3a88987d0e0ddd9434b308b54fa9871b227c134
zwergdev <zwergdev@gmail.com>
2024-11-28 18:30:08 +0100

    add new script

--- README.md
+++ README.md
@@ -79,3 +79,29 @@
     2b. Next, you have to allow the `RoyaltyModule` to spend those tokens on your behalf so it can properly distribute royalties to ancestor IPs. Run the [approve transaction](https://odyssey.storyscan.xyz/address/0xC0F6E387aC0B324Ec18EAcf22EE7271207dCE3d5?tab=write_contract#0x095ea7b3) where the `spender` is `0xEa6eD700b11DfF703665CCAF55887ca56134Ae3B` (this is the Odyssey v1.2 address of `RoyaltyModule` found [here](https://docs.story.foundation/docs/deployed-smart-contracts)) and the value is >= 2 (that's the amount we're paying in the script).
 
 3. `npm run register-deriv-com`
+
+## ⚡ Run "Register Derivative Commercial (Short)" Example
+
+1. Add your Story Network Testnet wallet's private key to `.env` file:
+
+    ```
+    WALLET_PRIVATE_KEY=YOUR_WALLET_PRIVATE_KEY
+    ```
+
+2. You will be paying for the License Token using a [test ERC-20 token](https://odyssey.storyscan.xyz/address/0xC0F6E387aC0B324Ec18EAcf22EE7271207dCE3d5).
+
+    2a. Mint some tokens by running [this](https://odyssey.storyscan.xyz/address/0xC0F6E387aC0B324Ec18EAcf22EE7271207dCE3d5?tab=write_contract#0x40c10f19) transaction (10 is good).
+
+    2b. Next, you have to allow the `RoyaltyModule` to spend those tokens on your behalf so it can properly distribute royalties to ancestor IPs. Run the [approve transaction](https://odyssey.storyscan.xyz/address/0xC0F6E387aC0B324Ec18EAcf22EE7271207dCE3d5?tab=write_contract#0x095ea7b3) where the `spender` is `0xEa6eD700b11DfF703665CCAF55887ca56134Ae3B` (this is the Odyssey v1.2 address of `RoyaltyModule` found [here](https://docs.story.foundation/docs/deployed-smart-contracts)) and the value is >= 2 (that's the amount we're paying in the script).
+
+3. [Optional] Create a new SPG NFT collection by running `npm run create-spg-collection` in your terminal.
+
+    3a. Look at the console output, and copy the NFT contract address. Add that value as `SPG_NFT_CONTRACT_ADDRESS` to your `.env` file:
+
+    ```
+    SPG_NFT_CONTRACT_ADDRESS=SPG_NFT_CONTRACT_ADDRESS
+    ```
+
+    **NOTE: You will only have to do this one time. Once you create an SPG collection, you can run this script as many times as you'd like.**
+
+4. `npm run register-deriv-com-short`
---
+++ scripts/registerDerivativeCommercialShort.ts
@@ -0,0 +1,105 @@
+import {
+  PayRoyaltyOnBehalfResponse,
+  PIL_TYPE,
+  RegisterIpAndAttachPilTermsResponse,
+  StoryClient,
+  StoryConfig,
+  TransferToVaultAndSnapshotAndClaimByTokenBatchResponse,
+  MintAndRegisterIpAndMakeDerivativeResponse,
+} from '@story-protocol/core-sdk'
+import { Address, http, toHex, zeroAddress } from 'viem'
+import { SUSDAddress, RPCProviderUrl, RoyaltyPolicyLAP, account, SPGNFTContractAddress } from './utils/utils'
+
+// BEFORE YOU RUN THIS FUNCTION: Make sure to read the README which contains
+// instructions for running this "Register Derivative Commercial" example.
+
+const main = async function () {
+    // 1. Set up your Story Config
+    //
+    // Docs: https://docs.story.foundation/docs/typescript-sdk-setup
+    const config: StoryConfig = {
+        account: account,
+        transport: http(RPCProviderUrl),
+        chainId: 'odyssey',
+    }
+    const client = StoryClient.newClient(config)
+
+    // 2. Mint and Register an IP Asset
+    //
+    // Docs: https://docs.story.foundation/docs/register-an-nft-as-an-ip-asset
+    const parentIp: RegisterIpAndAttachPilTermsResponse = await client.ipAsset.mintAndRegisterIpAssetWithPilTerms({
+        spgNftContract: SPGNFTContractAddress,
+        pilType: PIL_TYPE.COMMERCIAL_REMIX,
+        commercialRevShare: 50, // 50%
+        mintingFee: 0,
+        currency: SUSDAddress,
+        // NOTE: The below metadata is not configured properly. It is just to make things simple.
+        // See `simpleMintAndRegister.ts` for a proper example.
+        ipMetadata: {
+            ipMetadataURI: 'test-uri',
+            ipMetadataHash: toHex('test-metadata-hash', { size: 32 }),
+            nftMetadataHash: toHex('test-nft-metadata-hash', { size: 32 }),
+            nftMetadataURI: 'test-nft-uri',
+        },
+        txOptions: { waitForTransaction: true },
+    })
+    console.log(
+        `Root IPA created at transaction hash ${parentIp.txHash}, IPA ID: ${parentIp.ipId}, License Terms ID: ${parentIp.licenseTermsId}`
+    )
+
+    // 3. Mint and Register IP asset and make it a derivative of the parent IP Asset
+    //
+    // Docs #1: https://docs.story.foundation/docs/register-an-nft-as-an-ip-asset
+    // Docs #2: https://docs.story.foundation/docs/register-a-derivative#existing-child-ip--parent-ip
+    const { txHash, childIpId }: MintAndRegisterIpAndMakeDerivativeResponse = await client.ipAsset.mintAndRegisterIpAndMakeDerivative({
+        spgNftContract: SPGNFTContractAddress,
+        derivData: {
+            parentIpIds: [parentIp.ipId as Address],
+            licenseTermsIds: [parentIp.licenseTermsId as bigint],
+        },
+        // NOTE: The below metadata is not configured properly. It is just to make things simple.
+        // See `simpleMintAndRegister.ts` for a proper example.
+        ipMetadata: {
+            ipMetadataURI: 'test-uri',
+            ipMetadataHash: toHex('test-metadata-hash', { size: 32 }),
+            nftMetadataHash: toHex('test-nft-metadata-hash', { size: 32 }),
+            nftMetadataURI: 'test-nft-uri',
+        },
+        txOptions: { waitForTransaction: true },
+    })
+    console.log(`Derivative IPA created and linked at transaction hash ${txHash}, IPA ID: ${childIpId}}`)
+
+    // 4. Pay Royalty
+    // NOTE: You have to approve the RoyaltyModule to spend 2 SUSD on your behalf first. See README for instructions.
+    //
+    // Docs: https://docs.story.foundation/docs/pay-ipa
+    const payRoyalty: PayRoyaltyOnBehalfResponse = await client.royalty.payRoyaltyOnBehalf({
+        receiverIpId: childIpId as Address,
+        payerIpId: zeroAddress,
+        token: SUSDAddress,
+        amount: 2,
+        txOptions: { waitForTransaction: true },
+    })
+    console.log(`Paid royalty at transaction hash ${payRoyalty.txHash}`)
+
+    // 5. Claim Revenue
+    //
+    // Docs: https://docs.story.foundation/docs/claim-revenue
+    const claimRevenue: TransferToVaultAndSnapshotAndClaimByTokenBatchResponse =
+        await client.royalty.transferToVaultAndSnapshotAndClaimByTokenBatch({
+            ancestorIpId: parentIp.ipId as Address,
+            claimer: parentIp.ipId as Address,
+            royaltyClaimDetails: [
+                {
+                    childIpId: childIpId as Address,
+                    royaltyPolicy: RoyaltyPolicyLAP,
+                    currencyToken: SUSDAddress,
+                    amount: 1,
+                },
+            ],
+            txOptions: { waitForTransaction: true },
+        })
+    console.log(`Claimed revenue: ${claimRevenue.amountsClaimed} at snapshotId ${claimRevenue.snapshotId}`)
+}
+
+main()
