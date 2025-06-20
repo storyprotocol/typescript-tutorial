334d98a09f646ea949fa5d6abe6cc040a73a45ef
jacob-tucker <jacobtucker818@gmail.com>
2025-06-19 23:45:44 -0400

    add claimablerevenue

--- scripts/royalty/claimRevenue.ts
+++ scripts/royalty/claimRevenue.ts
@@ -6,17 +6,29 @@ import { client } from '../../utils/config'
 const IP_ID: Address = '0x641E638e8FCA4d4844F509630B34c9D524d40BE5'
 
 const main = async function () {
-    // 1. Claim Revenue
-    //
-    // Docs: https://docs.story.foundation/sdk-reference/royalty#claimallrevenue
-    const response = await client.royalty.claimAllRevenue({
-        ancestorIpId: IP_ID,
+    // 1. Check if there is any revenue to claim
+    const claimableRevenue = await client.royalty.claimableRevenue({
+        ipId: IP_ID,
         claimer: IP_ID,
-        currencyTokens: [WIP_TOKEN_ADDRESS],
-        childIpIds: [],
-        royaltyPolicies: [],
+        token: WIP_TOKEN_ADDRESS,
     })
-    console.log('Claimed revenue:', response.claimedTokens)
+    console.log('Claimable revenue:', claimableRevenue)
+
+    if (claimableRevenue > 0) {
+        // 2. Claim Revenue
+        //
+        // Docs: https://docs.story.foundation/sdk-reference/royalty#claimrevenue
+        const claim = await client.royalty.claimAllRevenue({
+            ancestorIpId: IP_ID,
+            claimer: IP_ID,
+            currencyTokens: [WIP_TOKEN_ADDRESS],
+            childIpIds: [],
+            royaltyPolicies: [],
+        })
+        console.log('Claimed revenue:', claim.claimedTokens)
+    } else {
+        console.log('No revenue to claim')
+    }
 }
 
 main()
